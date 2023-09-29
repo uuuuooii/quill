eb166925aeb7a34917bfe876d0aaa35bf63e1351
neo@zillinks.com <keunhey0107@gmail.com>
2023-09-29 21:51:35 +0900

    Update: Main 페이지 폴더 분리

---
+++ src/app/main/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import InfoSection from './info'
+import PropositionSection from './proposition'
+
+const Main = () => {
+  return (
+    <>
+      <InfoSection />
+      <PropositionSection />
+    </>
+  )
+}
+
+export default Main
