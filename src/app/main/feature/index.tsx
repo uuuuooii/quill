2ca8194060fb620a2cb272b0ed8b99b795d09f40
neo@zillinks.com <keunhey0107@gmail.com>
2023-09-30 16:38:26 +0900

    Update: uploading imge 추가

--- src/app/main/feature/index.tsx
+++ src/app/main/feature/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FirstStep from './step1'
 import SecondStep from './step2'
 import ThirdStep from './step3'
+import Image from 'next/image'
 
 const FeatureSection = () => {
   return (
@@ -21,6 +22,21 @@ const FeatureSection = () => {
         <SecondStep />
         <ThirdStep />
       </ol>
+
+      <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+        <div className='mt-16 flow-root sm:mt-24'>
+          <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+            <Image
+              src='/file-upload-preview.jpg'
+              alt='uploading preview'
+              width={1364}
+              height={866}
+              quality={100}
+              className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
