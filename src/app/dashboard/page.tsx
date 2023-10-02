94fc596ba0bc93f80cba2671afdb97c5c4537229
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-02 14:49:40 +0900

    Update: dashboard 추가

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
+import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Dashboard = () => {
+const Dashboard = async () => {
   const { getUser } = getKindeServerSession()
   const user = getUser()
 
@@ -10,8 +11,18 @@ const Dashboard = () => {
     redirect('/auth-callback?origin=dashboard')
   }
 
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
+  console.log(dbUser)
+  if (!dbUser) {
+    redirect('/auth-callback?origin=dashboard')
+  }
+
   return (
-    <div>{user.email}</div>
+    <Dashboard />
   )
 }
 
