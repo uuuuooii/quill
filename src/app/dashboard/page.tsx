fd780fa45358bc1d04a1b402ded94cee7c725ea3
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-03 20:59:44 +0900

    Update: uploadButton

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,29 +1,30 @@
+import Dashboard from '@/components/dashboard'
 import { db } from '@/lib/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const Dashboard = async () => {
+const DashboardPage = async () => {
   const { getUser } = getKindeServerSession()
   const user = getUser()
 
-  if (!user || !user.id) {
-    redirect('/auth-callback?origin=dashboard')
-  }
+  // if (!user || !user.id) {
+  //   redirect('/auth-callback?origin=dashboard')
+  // }
 
-  const dbUser = await db.user.findFirst({
-    where: {
-      id: user.id
-    }
-  })
-  console.log(dbUser)
-  if (!dbUser) {
-    redirect('/auth-callback?origin=dashboard')
-  }
+  // const dbUser = await db.user.findFirst({
+  //   where: {
+  //     id: user.id
+  //   }
+  // })
+  // console.log(dbUser)
+  // if (!dbUser) {
+  //   redirect('/auth-callback?origin=dashboard')
+  // }
 
   return (
     <Dashboard />
   )
 }
 
-export default Dashboard
+export default DashboardPage
---
+++ src/components/uploadButton/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from 'react'
+
+const UploadButton = () => {
+  return (
+    <div>UploadButton</div>
+  )
+}
+
+export default UploadButton
