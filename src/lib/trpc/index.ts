a1513f57fd8b1a14f49f05488edd0df7527dd057
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-17 21:39:05 +0900

    Update: prisma schema 추가

--- src/lib/trpc/index.ts
+++ src/lib/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { publicProcedure, router } from './trpc';
+import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/lib/db';
 
@@ -32,6 +32,15 @@ export const appRouter = router({
     }
 
     return { success: true };
+  }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId, user } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
   }),
 });
 
